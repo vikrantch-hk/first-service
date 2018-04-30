498accfa32d5a2264b8c11be8ae504d699b99af9
vikrantch-hk <chudhry.vikrant@Brightlife.com>
2018-04-30 15:27:37 +0530

    1

---
+++ src/main/java/com/devglan/firstservice/controller/FirstController.java
@@ -0,0 +1,19 @@
+package com.devglan.firstservice.controller;
+
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+   /* @RequestMapping("/")
+    public String demo(@RequestHeader("X-source") String source){
+        return "First service with " + source;
+    }*/
+
+    @RequestMapping("/")
+    public String demo(){
+        return "First service with ";
+    }
+}
